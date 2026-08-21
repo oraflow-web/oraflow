235ac0669cb93b9cfccd3535b1d1a3f61f93f327
Marija <>
2026-08-21 21:47:44 +0200

    styles added

--- apps/studio/sanity.config.ts
+++ apps/studio/sanity.config.ts
@@ -1,18 +1,18 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemaTypes";
 
 export default defineConfig({
-  name: 'default',
-  title: 'OraFlow',
+  name: "default",
+  title: "OraFlow",
 
-  projectId: 'awd005lt',
-  dataset: 'production',
+  projectId: "awd005lt",
+  dataset: "production",
 
   plugins: [structureTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
   },
-})
+});
